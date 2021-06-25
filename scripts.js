fb70012d8bf80f0f947090de0f4d6f22980e662b
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-06-25 17:23:27 -0500

    Funciona bien calcular altura triangulo
    se implemento funciones para calcular la altura, imprimir, limpiar, validar si existe el triangulo :3

--- scripts.js
+++ scripts.js
@@ -1,35 +1,140 @@
 const calcularAreaCuadrado = () => {
-    let input = document.querySelector('#ladoCuadrado')
-    let value = input.value
-    let area = (value*value)+"cm²"
+  let input = document.querySelector("#ladoCuadrado");
+  let value = input.value;
+  let area = value * value + "cm²";
 
+  let resultBox = document.querySelector(".areaResultado__text");
 
-    let resultBox = document.querySelector('.areaResultado__text')
+  console.log(resultBox);
 
-    console.log(resultBox);
+  if (resultBox) {
+    resultBox.remove();
+  }
 
-    if (resultBox) {
-        resultBox.remove()
+  let resultNode = document.createElement("p");
+  resultNode.className = "areaResultado__text";
 
-    }
+  let nodoPadre = document.querySelector(".areaResultado");
 
+  nodoPadre.append(resultNode);
 
-    let resultNode = document.createElement('p')
-    resultNode.className = "areaResultado__text"
+  resultBox = document.querySelector(".areaResultado__text");
+  resultBox.append(area);
+};
 
-    let nodoPadre = document.querySelector('.areaResultado')
+const calcularAlturaTriangle = () => {
+  let ladoA = document.querySelector("#ladoTriangleA");
+  let valueA = ladoA.value;
 
-    nodoPadre.append(resultNode)
+  let ladoB = document.querySelector("#ladoTriangleB");
+  let valueB = ladoB.value;
 
-    resultBox = document.querySelector('.areaResultado__text')
-    resultBox.append(area)
-}
+  let ladoC = document.querySelector("#ladoTriangleC");
+  let valueC = ladoC.value;
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('input').forEach( node => node.addEventListener('keypress', e => {
-      if(e.keyCode == 13) {
-        e.preventDefault();
-        calcularAreaCuadrado();
+  let base, ladoIgual, alturaTriangle;
+
+  printAlert("");
+
+  if (valueA > 0 && valueB > 0 && valueC > 0) {
+    // console.log(valueA+valueB+valueC);
+    if (valueA == valueB || valueA == valueC || valueB == valueC) {
+      if (valueA == valueB) {
+        ladoIgual = valueA;
+        base = valueC;
+
+        if (ladoIgual * 2 <= base) {
+          printAlert("Tu triangulo es imposible");
+        } else if (ladoIgual * 2 >= base) {
+          alturaTriangle = Math.sqrt(
+            ladoIgual * ladoIgual - (base / 2) * (base / 2)
+          );
+          printResult(alturaTriangle);
+        }
+      } else if (valueA == valueC) {
+        ladoIgual = valueA;
+        base = valueB;
+
+        if (ladoIgual * 2 <= base) {
+          printAlert("Tu triangulo es imposible");
+        } else if (ladoIgual * 2 >= base) {
+          alturaTriangle = Math.sqrt(
+            ladoIgual * ladoIgual - (base / 2) * (base / 2)
+          );
+          printResult(alturaTriangle);
+        }
+      } else if (valueB == valueC) {
+        ladoIgual = valueB;
+        base = valueA;
+
+        if (ladoIgual * 2 <= base) {
+          printAlert("Tu triangulo es imposible");
+        } else if (ladoIgual * 2 >= base) {
+          alturaTriangle = Math.sqrt(
+            ladoIgual * ladoIgual - (base / 2) * (base / 2)
+          );
+          printResult(alturaTriangle);
+        }
       }
-    }))
-  });
+    } else {
+      printAlert("No hay 2 lados iguales")
+    }
+  } else {
+    console.log("Uno de los valores es menor que cero");
+  }
+};
+
+//Previene que se envie la data al presionar ENTERN
+//en todos los input
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("input").forEach((node) =>
+    node.addEventListener("keypress", (e) => {
+      if (e.keyCode == 13) {
+        e.preventDefault();
+        // calcularAreaCuadrado();
+      }
+    })
+  );
+});
+
+const printResult = (info) => {
+  //console.log("Desde printResult");
+  let resultBoxT = document.querySelector(".areaResultadoT__text");
+  if (resultBoxT) {
+    resultBoxT.remove();
+  }
+  let createRBT = document.createElement("p");
+  createRBT.className = "areaResultadoT__text";
+  createRBT.append(info.toFixed(3) + "cm");
+  let nodoPadre = document.querySelector(".areaResultadoT");
+  nodoPadre.append(createRBT);
+};
+
+const printAlert = (info) => {
+
+  //console.log("Desde printResult");
+  // let resultBoxT = document.querySelector(".areaResultadoT__textAlert");
+  // if (resultBoxT) {
+  //   resultBoxT.remove();
+  // }
+
+  limpiarPrints();
+
+  let createRBT = document.createElement("p");
+  createRBT.className = "areaResultadoT__textAlert";
+  createRBT.append(info);
+  let nodoPadre = document.querySelector(".areaResultadoT");
+  nodoPadre.append(createRBT);
+};
+
+const limpiarPrints = () => {
+  let resultadoNum=document.querySelector('.areaResultadoT__text');
+  let textAlert=document.querySelector('.areaResultadoT__textAlert');
+
+  if (resultadoNum) {
+    resultadoNum.remove()
+  }
+  if (textAlert) {
+    textAlert.remove()
+  }
+}
