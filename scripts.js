8d66657d429698876fda1dbd05eefb2b2a3abd4c
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-06-24 22:04:35 -0500

    Calculadora de AreaSquare lista
    HTML y CSS implementados

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const calcularAreaCuadrado = () => {
+    let input = document.querySelector('#ladoCuadrado')
+    let value = input.value
+
+    alert(`El area de tu cuadrado es: ${value*value}`)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('input').forEach( node => node.addEventListener('keypress', e => {
+      if(e.keyCode == 13) {
+        e.preventDefault();
+      }
+    }))
+  });
